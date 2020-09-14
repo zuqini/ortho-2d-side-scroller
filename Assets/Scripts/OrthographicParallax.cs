fc21cd51cb1582f9c1ff9c4773ba95eb8f5da209
zuqini <lizuqiliang@gmail.com>
2020-09-14 01:05:37 -0700

    Add basic camera, parallax code for boilerplate

---
+++ Assets/Scripts/OrthographicParallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrthographicParallax : MonoBehaviour
+{
+    private float length;
+    private Vector3 startPos;
+
+    public GameObject mainCamera;
+    public float parallaxScale;
+
+    void Start()
+    {
+       startPos = transform.position;
+       length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate() 
+    {
+        var cameraPos = mainCamera.transform.position;
+        var dist = cameraPos * parallaxScale;
+
+        if (cameraPos.x > startPos.x + dist.x + length)
+        {
+            startPos.x += length;
+        }
+        else if (cameraPos.x < startPos.x + dist.x - length) 
+        {
+            startPos.x -= length;
+        }
+
+        transform.position = new Vector3(startPos.x + dist.x, startPos.y + dist.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 controlAxis = new Vector2(0f, 0f);
+    private Vector2 velocity = new Vector2(0f, 0f);
+    private SpriteRenderer spriteRenderer;
+
+    public float acceleration = 1f;
+    public float resistance = 0.1f;
+
+    void Update()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        controlAxis.x = Input.GetAxisRaw("Horizontal");
+        controlAxis.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // apply acceleration
+        velocity += acceleration * controlAxis * Time.deltaTime;
+        spriteRenderer.flipX = velocity.x < 0;
+
+        // apply velocity
+        transform.position = transform.position + (Vector3) velocity;
+
+        // apply air Resistance
+        velocity = (1 - resistance) * velocity;
+    }
+}
